cf9eb4c443121a5024485890fb608ab7f2691137
fredbec <f-becker@posteo.de>
2022-11-16 18:14:47 +0100

    shell option for target_date

--- R/utils.R
+++ R/utils.R
@@ -19,6 +19,16 @@ getquants <- function(tser,
 
 
 
+#' Title
+#'
+#' @param preds
+#' @param currDate
+#' @param weekday
+#' @param nameSeries
+#'
+#' @return a data.table corresponding to the challenge's format requirements
+#'
+#'
 daxshell <- function(preds,
                      currDate,
                      weekday = "Wednesday",
@@ -41,14 +51,27 @@ daxshell <- function(preds,
   preds <- preds |>
     .d(, forecast_date := rep(currDate, maxhor)) |>
     .d(, target := rep(nameSeries, maxhor)) |>
-    .d(, horizon := horizons)
+    .d(, horizon := horizons) |>
+    setcolorder(c("forecast_date", "target", "horizon", paste0("q", c(0.025, 0.25, 0.5, 0.75, 0.975))))
 
   return(preds)
 }
 
 
+#' Title
+#'
+#' @param preds
+#' @param currDate
+#' @param weekday
+#' @param nameSeries
+#'
+#' @return a data.table corresponding to the challenge's format requirements
+#'
+#'
 energyshell <- function(preds,
                         currDate,
+                        targetFullDate = FALSE,
+                        targetDiff = c(2,3),
                         weekday = "Wednesday",
                         nameSeries = "energy"){
 
@@ -58,14 +81,31 @@ energyshell <- function(preds,
 
   horizons <- paste0(c(36,40,44,60,64,68), " hour")
 
+  if(targetFullDate){
+    FSDates <- c(as.Date(currDate) + lubridate::days(targetDiff[1]),
+                 as.Date(currDate) + lubridate::days(targetDiff[2]))
+    FSTimes <- c("11:00:00", "15:00:00", "19:00:00")
+
+
+    targetDates <- c(lubridate::as_datetime(paste(FSDates[1], FSTimes)),
+                     lubridate::as_datetime(paste(FSDates[2], FSTimes)))
+  }
+
   preds <- preds |>
     .d(, forecast_date := currDate) |>
     .d(, target := rep(nameSeries, maxhor)) |>
     .d(, horizon := horizons) |>
-    setcolorder(neworder = c("forecast_date", "target", "horizon", paste0("q", c(0.025, 0.25, 0.5, 0.75, 0.975))))
+    setcolorder(neworder = c("forecast_date", "target", "horizon",
+                             paste0("q", c(0.025, 0.25, 0.5, 0.75, 0.975))))
 
+  if(targetFullDate){
+    preds |>
+      .d(, target_date := targetDates)
+  }
   return(preds)
 }
+
+
 
 
 windshell <- function(preds,
